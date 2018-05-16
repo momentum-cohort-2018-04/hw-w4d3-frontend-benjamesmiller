953a503a2340132d2a81fd8c17552e0d4721b30f
benjamesmiller <benjamesmiller@gmail.com>
2018-05-16 15:24:27 -0400

    Started setting up js

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
 import request from "superagent"
 
+let notes = []
+
+function id (thatString) {
+    return document.getElementById(thatString)
+}
+
+function glitchAPI (query) {
+    return `https://notes-api.glitch.me/api/${query}`
+}
+
+function getAndUpdateNotes () {
+    request 
+    .get(glitchAPI('notes'))
+    .auth('benjamesmiller', 'nash13')
+    .then(Response => {
+        notes = Response.body.notes
+        updateNotes(notes)
+    })
+}
+
+function updateNotes (divID, notes) {
+    const div = id(divID)
+    const bodyNotes = section.querySelector('note-dis')
+    bodyNotes.innerHTML = notesToHTML(notes)
+}
+
+function notesToHTML (notes) {
+    return notes.map(notesToHTML).join('')
+}
+
+function notetoHTML (note) {
+    return `<div data-note-id="${note._id}"></div>
+    <button type="button" class="button-delete button-danger" data-note-id="${note._id}">Delete</button>
+    <button type="button" class="button-delete button-warning" data-note-id="${note._id}">Edit</button>`
+}
